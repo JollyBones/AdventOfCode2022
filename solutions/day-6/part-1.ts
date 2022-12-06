5ecdd2a290bd4b7e2572bb6a74dd0746e49d9ee6
Aidan Jolly <aidanjolly9@gmail.com>
2022-12-06 21:19:22 +0800

    Refactored

--- solutions/day-6/part-1.ts
+++ solutions/day-6/part-1.ts
@@ -1,10 +1,11 @@
 import { readInFile } from '../filereader';
 
+const WINDOW_SIZE = 4;
+
 readInFile('./inputs/day-6/input.dat', (data) => {
-	const WINDOW_SIZE = 4;
 	let index = 0;
 	let startOfPacket = -1;
-	console.log(data.length);
+
 	while (startOfPacket < 0) {
 		const substring = data.slice(index, index + WINDOW_SIZE);
 		if (new Set(substring).size === 4) {
