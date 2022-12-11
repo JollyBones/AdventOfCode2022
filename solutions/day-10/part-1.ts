c5f49271442dfb7d3cf636a352d6c4991d9f29ff
Aidan Jolly <aidanjolly9@gmail.com>
2022-12-11 13:35:25 +0800

    Completed part 1 of day 10

--- solutions/day-10/part-1.ts
+++ solutions/day-10/part-1.ts
@@ -1,146 +1,40 @@
-addx 15
-addx - 11
-addx 6
-addx - 3
-addx 5
-addx - 1
-addx - 8
-addx 13
-addx 4
-noop
-addx - 1
-addx 5
-addx - 1
-addx 5
-addx - 1
-addx 5
-addx - 1
-addx 5
-addx - 1
-addx - 35
-addx 1
-addx 24
-addx - 19
-addx 1
-addx 16
-addx - 11
-noop
-noop
-addx 21
-addx - 15
-noop
-noop
-addx - 3
-addx 9
-addx 1
-addx - 3
-addx 8
-addx 1
-addx 5
-noop
-noop
-noop
-noop
-noop
-addx - 36
-noop
-addx 1
-addx 7
-noop
-noop
-noop
-addx 2
-addx 6
-noop
-noop
-noop
-noop
-noop
-addx 1
-noop
-noop
-addx 7
-addx 1
-noop
-addx - 13
-addx 13
-addx 7
-noop
-addx 1
-addx - 33
-noop
-noop
-noop
-addx 2
-noop
-noop
-noop
-addx 8
-noop
-addx - 1
-addx 2
-addx 1
-noop
-addx 17
-addx - 9
-addx 1
-addx 1
-addx - 3
-addx 11
-noop
-noop
-addx 1
-noop
-addx 1
-noop
-noop
-addx - 13
-addx - 19
-addx 1
-addx 3
-addx 26
-addx - 30
-addx 12
-addx - 1
-addx 3
-addx 1
-noop
-noop
-noop
-addx - 9
-addx 18
-addx 1
-addx 2
-noop
-noop
-addx 9
-noop
-noop
-noop
-addx - 1
-addx 2
-addx - 37
-addx 1
-addx 3
-noop
-addx 15
-addx - 21
-addx 22
-addx - 6
-addx 1
-noop
-addx 2
-addx 1
-noop
-addx - 10
-noop
-noop
-addx 20
-addx 1
-addx 2
-addx 2
-addx - 6
-addx - 11
-noop
-noop
-noop
+import { readInFile } from "../filereader";
+
+readInFile('./inputs/day-10/input.dat', (data) => {
+	let cycle = 1;
+	let x = 1;
+
+	const signalStrengths: number[] = [];
+	const inputs = data.split('\n');
+
+	const emitSignalStrength = () => signalStrengths.push(cycle * x);
+
+	const getSignalPosition = (cycle: number): number => (cycle + 20) % 40;
+
+	inputs.forEach(input => {
+		if (input === "noop") {
+			cycle++;
+			if (getSignalPosition(cycle) === 0) {
+				emitSignalStrength()
+			}
+		} else {
+			const value = Number(input.split(' ')[1]);
+			const futureSignalPosition = getSignalPosition(cycle + 2);
+			if (futureSignalPosition === 1) {
+				cycle++;
+				emitSignalStrength();
+				x += value;
+				cycle++;
+			} else {
+				cycle += 2;
+				x += value;
+				if (futureSignalPosition === 0) {
+					emitSignalStrength();
+				}
+			}
+		}
+	});
+
+	console.log(signalStrengths);
+	console.log(signalStrengths.reduce((a, b) => a + b, 0));
+});
