9afdf6bc1d48491e3aadc22a793c33d92a355a20
Aidan <aidanjolly9@gmail.com>
2022-12-14 00:01:31 +0800

    Added testable first solution

---
+++ solutions/day-12/part-1.ts
@@ -0,0 +1,113 @@
+import { readInFile } from '../filereader.ts';
+
+type Point = {
+	x: number;
+	y: number;
+};
+let MAX_X = -1;
+let MAX_Y = 1;
+let grid: string[][] = [];
+
+const getStartAndEndPoints = (grid: string[][]): Point[] => {
+	let start: Point | undefined = undefined;
+	let end: Point | undefined = undefined;
+	
+	points: for (let rowIndex = 0; rowIndex < grid.length; rowIndex++) {
+		if (start && end) {
+			break points;
+		}
+		const row = grid[rowIndex];
+		const startIndex = row.findIndex(cell => cell === 'S');
+		const endIndex = row.findIndex(cell => cell === 'E');
+
+		if (startIndex > -1) {
+			start = {
+				x: startIndex,
+				y: rowIndex
+			}
+		}
+		if (endIndex > -1) {
+			end = {
+				x: endIndex,
+				y: rowIndex
+			}
+		}
+	}
+	return [start!, end!];
+}
+
+const isSamePoint = (a: Point, b: Point) => a.x === b.x && a.y === b.y;
+
+const getValidSteps = (curr: Point, pastPoints: Point[]) => {
+	const baseSteps = [
+		{
+			...curr,
+			x: curr.x - 1
+		},
+		{
+			...curr,
+			x: curr.x + 1
+		},
+		{
+			...curr,
+			y: curr.y - 1,
+		},
+		{
+			...curr,
+			y: curr.y + 1,
+		}
+	];
+
+	return baseSteps
+		.filter(step => !(step.x < 0 || step.y < 0) && !(step.x > MAX_X || step.y > MAX_Y))
+		.filter(step => !pastPoints.find(p => isSamePoint(step, p)));
+}
+
+const sanitiseStep = (code: number): number => {
+	if (code === 83) return 97;
+	if (code === 69) return 122;
+	return code;
+}
+
+const isSafeStep = (curr: Point, target: Point): boolean => {
+	const currHeight = sanitiseStep(grid[curr.y][curr.x].charCodeAt(0));
+	const targetHeight = sanitiseStep(grid[target.y][target.x].charCodeAt(0));
+
+	return Math.abs(currHeight - targetHeight) < 2;
+};
+
+const findPaths = (
+	start: Point, 
+	end: Point, 
+	pastPoints: Point[] = [],
+	stepCount = 0,
+): number[] => {
+	if (isSamePoint(start, end)) {
+		console.log(`Found valid path in ${stepCount} steps`);
+		return [stepCount];
+	}
+
+	const steps = getValidSteps(start, pastPoints);
+	return steps.map(step => {
+		if (isSafeStep(start, step)) {
+			return findPaths(step, end, [...pastPoints, start], stepCount + 1)
+		}
+		console.log('Dead end :/')
+		return [];
+	}).flatMap(a => a, []);
+}
+
+readInFile('./inputs/day-12/input.dat', (data) => {
+	// Set globals;
+	grid = data.split("\n").map(d => d.split(""));
+	MAX_X = grid[0].length - 1;
+	MAX_Y = grid.length - 1;
+
+	// Get starts and ends
+	const [ start, end ] = getStartAndEndPoints(grid);
+
+	// Get paths
+	const paths = findPaths(start, end);
+
+	console.log(paths.sort());
+});
