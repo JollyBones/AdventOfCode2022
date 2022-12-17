862fb2562365d602866a4488d57f89c06e76adc2
Aidan <aidanjolly9@gmail.com>
2022-12-18 01:13:24 +0800

    slow working solution

--- solutions/day-15/part-2.ts
+++ solutions/day-15/part-2.ts
@@ -4,53 +4,44 @@ function calcManDist(senX: number, senY: number, beaX: number, beaY: number) {
 	return (Math.abs(senX - beaX) + Math.abs(senY - beaY));
 }
 
-function checkImpossibleLocations(distances: Map<string, number>, searchSize: number): Map<number, Set<number>> {
-	const impossibles = new Map<number, Set<number>>();
-
-	distances.forEach((dist, coord) => {
-		const [xBase, yBase] = coord.match(/\d+/g)!.map(v => Number(v));
-
-		const minY = Math.max(0, yBase - dist);
-		const maxY = Math.min(searchSize, yBase + dist);
-
-		console.log(coord, minY, maxY);
-		for (let y = minY; y <= maxY; y++) {
-			const xOffset = Math.abs(Math.abs(y - yBase) - dist)
-			const minX = Math.max(0, xBase - xOffset);
-			const maxX = Math.min(searchSize, xBase + xOffset);
-			const set = impossibles.get(y) ?? new Set();
-			for (let x = minX; x <= maxX; x++) {
-				set.add(x);
-			}
-			impossibles.set(y, set);
-		}
+function checkImpossibleLocations(sensors: Map<string, number>, searchSize: number): string {
+	const distances = [...sensors.entries()].map(s => {
+		const [x, y] = s[0].match(/[-\d]+/g)!.map(s => Number(s));
+		return {
+			x,
+			y,
+			distance: s[1],
+		};
 	});
-	return impossibles;
+	for (let x = 0; x <= searchSize; x++) {
+		for (let y = 0; y <= searchSize; y++) {
+			let found = false;
+			dLoop: for (let ii = 0; ii < distances.length; ii++) {
+				const distance = distances[ii];
+				if (calcManDist(x, y, distance.x, distance.y) <= distance.distance) {
+					found = true;
+					break dLoop;
+				}
+			}
+			if (!found) {
+				return `${x},${y}`;
+			}
+		}
+	}
+	return "UNFOUND";
 }
 
 readInFile('./inputs/day-15/input.dat', (data) => {
 	const inputs = data.split('\n');
 	const distances = new Map<string, number>();
-	const searchSize = 10_000; // Change this to 10,000;
+	const searchSize = 400_000;
 
 	inputs.forEach(input => {
 		const [senX, senY, beaX, beaY] = input.match(/[-\d]+/g)!.map(value => Number(value));
 		distances.set(`${senX},${senY}`, calcManDist(senX, senY, beaX, beaY));
 	});
 
-	const impossibles = checkImpossibleLocations(distances, searchSize);
-
+	const distressSignal = checkImpossibleLocations(distances, searchSize);
+	console.log(distressSignal);
 	// Tune frequency.
-
-	const possibles = [];
-	for (let y = 0; y <= searchSize; y++) {
-		const row = impossibles.get(y) ?? new Set();
-		for (let x = 0; x <= searchSize; x++) {
-			if (!row.has(x)) {
-				possibles.push(`${x},${y}`);
-			}
-		}
-	}
-	console.log(impossibles.size);
-	console.log(possibles);
 });
